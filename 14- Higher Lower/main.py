d2f3e31d20db541206ae26ccca454b641d0b8139
brunombs <99062708+brunombs@users.noreply.github.com>
2023-05-30 04:48:04 +0100

    Add files via upload

--- 14- Higher Lower/main.py
+++ 14- Higher Lower/main.py
@@ -8,13 +8,15 @@ score = 0
 while game_on:
     a = random.choice(game_data.data)
     b = random.choice(game_data.data)
+    if a == b:
+        b = random.choice(game_data.data)
     if a['follower_count'] > b['follower_count']:
         higher_score = 'a'
     else:
         higher_score = 'b'
     print(f"Compare A: {a['name']}, {a['description']}, from {a['country']}")
     print(art.vs)
-    print(f"Compare A: {b['name']}, {b['description']}, from {b['country']}")
+    print(f"Compare B: {b['name']}, {b['description']}, from {b['country']}")
     awnser = input("Who has more followers? Type 'A' or 'B': ").lower()
     while awnser != 'a' and awnser != 'b':
         awnser = input("Who has more followers? Type ONLY 'A' or 'B': ").lower()
